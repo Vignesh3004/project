38e7347beb1290f1e3805acb0e6a1bc00882f916
Vignesh3004 <99144752+Vignesh3004@users.noreply.github.com>
2022-10-16 13:30:50 +0530

    Update db.php

--- app/db.php
+++ app/db.php
@@ -2,7 +2,7 @@
 /*
  * Database settings
  */
-define("DB_SERVER", "localhost");
+define("DB_SERVER", "127.0.0.1");
 define("DB_NAME", "database");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
